dfa2f8b4814961e786636ba1781885dd9b32b17e
nickklssn <nickklassen3@gmail.com>
2023-07-14 12:08:30 +0200

    [TASK] integrate keycloak as openid client

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const {
+  getAuthUrl,
+  getCallbackParams,
+} = require("./keycloak/client.js");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+
+app.get("/", (_req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/login", async (req, res) => {
+  res.redirect(await getAuthUrl())
+
+});
+
+app.get("/login/cb", async (req, res) => {
+  console.log(await getCallbackParams(req))
+  res.send("Successfully authenticated!")
 });
 
 app.listen(PORT, () => {
