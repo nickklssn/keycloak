2b4d2094df56afe990085cbaa6b52420197bc469
nickklssn <nickklassen3@gmail.com>
2023-07-18 16:37:48 +0200

    [TASK & FIX] add validate token function and fix client error

--- keycloak/client.js
+++ keycloak/client.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
-const { Issuer, generators } = require("openid-client");
+const { Issuer } = require("openid-client");
 
 async function createClient() {
-  const keycloakIssuer = await Issuer.discover(process.env.KC_ISSUER);
+  const keycloakIssuer = await Issuer.discover("http://localhost:8080/realms/myrealm");
 
   const client = new keycloakIssuer.Client({
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    redirect_uris: [process.env.REDIRECT_URI],
-    response_types: [process.env.RESPONSE_TYPES],
+    client_id: "myclient",
+    client_secret: "xD0VuRQnZnGu0eDyGXEjn8yx52IbdO0A",
+    redirect_uris: ["http://localhost:3000/login/cb"],
+    response_types: ["code"],
     id_token_signing_alg_values_supported:
-      process.env.ID_TOKEN_SIGNING_ALG_VALUES_SUPPORTED,
+    "RS256",
   });
 
   return { client };
@@ -20,33 +20,28 @@ async function getCallbackParams(request) {
   return (await createClient()).client.callbackParams(request);
 }
 
-async function getAuthUrl(code_challenge) {
+async function getAuthUrl() {
   return (await createClient()).client.authorizationUrl({
     scope: "openid email profile",
-    code_challenge,
-    code_challenge_method: "S256"
-
   });
 }
 
-function generateCodeVerifier() {
-  return generators.codeVerifier();
-}
 
-function generateCodeChallenge(codeVerifier) {
-  return generators.codeChallenge(codeVerifier);
-}
-
-async function generateTokenset(callbackUri, params, codeVerifier) {
+async function generateTokenset(callbackUri, params) {
   const {client} = await createClient()
-  const tokenSet = await client.callback(callbackUri, params, {codeVerifier})
+  const tokenSet = await client.callback(callbackUri, params)
   return tokenSet
 }
 
+async function validateToken(accessToken){
+  const {client} = await createClient()
+  const response = client.userinfo(accessToken)
+  console.log(response)
+}
+
 module.exports = {
-  generateCodeChallenge,
-  generateCodeVerifier,
   getAuthUrl,
   getCallbackParams,
   generateTokenset,
+  validateToken
 };
