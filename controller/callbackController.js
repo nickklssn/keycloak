e674f54ecbea9041101661bb5a2464d0245c1005
nickklssn <nickklassen3@gmail.com>
2023-07-18 16:36:36 +0200

    [TASK] remove pkce elements for testing

--- controller/callbackController.js
+++ controller/callbackController.js
@@ -1,17 +1,22 @@
-const {getCallbackParams, generateTokenset, generateCodeChallenge} = require("../keycloak/client.js")
+const {
+  getCallbackParams,
+  generateTokenset,
+} = require("../keycloak/client.js");
 
-const callback = async (req, res, next) =>{
-    try{
-        const code_verifier = req.cookies.code_verifier
-        console.log(code_verifier)
-        const params = await getCallbackParams(req)
-        const tokenSet = await generateTokenset("http://localhost:3000/login/cb", params, {code_verifier})
-        console.log(params)
-        res.send("Successfully authenticated")
-    }
-    catch(err){
-        console.error(err.message)
-    }
-}
+const callback = async (req, res, next) => {
+  try {
+    const params = await getCallbackParams(req);
+    const tokenSet = await generateTokenset(
+      "http://localhost:3000/login/cb",
+      params
+    );
+    console.log(params, tokenSet);
+    res.cookie("tokenset", tokenSet, { httpOnly: true });
+    res.send("Successfully authenticated");
+    next();
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
-module.exports = callback
+module.exports = callback;
--- controller/loginController.js
+++ controller/loginController.js
@@ -1,15 +1,10 @@
 const {
-  generateCodeChallenge,
-  generateCodeVerifier,
   getAuthUrl,
 } = require("../keycloak/client.js");
 
 const login = async (_req, res, next) => {
   try {
-    const code_verifier = generateCodeVerifier();
-    const code_challenge = generateCodeChallenge(code_verifier);
-    res.cookie("code_verifier", code_verifier, { httpOnly: true });
-    res.redirect(await getAuthUrl(code_challenge));
+    res.redirect(await getAuthUrl());
     next();
   } catch (err) {
     console.error(err.message);
