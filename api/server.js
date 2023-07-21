715cf0a0940803697991e4c68f55b17d6fa18334
nickklssn <nickklassen3@gmail.com>
2023-07-21 14:54:33 +0200

    [TASK] add middleware to api

--- api/server.js
+++ api/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const verifyToken = require("./middleware/verifyToken");
+const verifyToken = require("./middleware/verifyToken.js");
+const authentication = require("./middleware/authentication.js");
 
 const app = express();
 
@@ -11,13 +12,15 @@ const PORT = process.env.API_PORT|| 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(verifyToken)
+app.use(authentication)
 
 
 app.get("/", (req, res) =>{
     res.send("Welcome to the api")
 })
 
-app.get("/data", verifyToken ,(req, res) =>{
+app.get("/data" ,(req, res) =>{
     res.json(({name: "Nick", age:23, gender: "m"}))
 })
 
