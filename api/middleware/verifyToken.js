6447c44428c763fdb7d8dd39db9baab0bac8268a
nickklssn <nickklassen3@gmail.com>
2023-07-21 14:53:16 +0200

    [TASK] add verifyToken middleware

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,22 +1,22 @@
-const {validateToken} = require("../../keycloak/client.js")
+const { isValid, regenerateToken } = require("../../keycloak/client.js");
 
 const verifyToken = async (req, res, next) => {
-    try{
-        const access_token = req.cookies.tokenset.access_token
-        console.log(access_token)
-        await validateToken(access_token)
-        next()
-
-
-        
-        
-
+  try {
+    const token = req.cookies.tokenset;
+    const valid = await isValid(token);
+    if (valid) {
+      console.log("Ist valide"); // Token ist gültig
+      next();
+    } else {
+      console.log("Ist nicht valide"); //Token ist nicht gültig und wird refresht
+      const newToken = await regenerateToken(token.refresh_token);
+      res.cookie("tokenset", newToken, { httpOnly: true });
+      next();
     }
+  } catch (err) {
+    console.error(err.message)
+    next(err)
+  }
+};
 
-catch(err){
-    console.error(err)
-    res.send(err.message)
-}
-}
-
-module.exports = verifyToken
+module.exports = verifyToken;
