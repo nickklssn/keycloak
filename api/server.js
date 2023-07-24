6209888206375a9fea1e65c64d2279ee58683a15
nickklssn <nickklassen3@gmail.com>
2023-07-24 12:52:52 +0200

    [TASK] create simple api for test purposes

---
+++ api/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+const PORT = process.env.API_PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the api");
+});
+
+app.get("/data", (req, res) => {
+  res.json({ name: "Nick", age: 23, gender: "m" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server listen on port 3001");
+});
