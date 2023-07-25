2d5b94f4ebbbceb15d4dfe266abd91dce7d87771
nickklssn <nickklassen3@gmail.com>
2023-07-25 16:47:39 +0200

    [TASK] store generated tokenset inside cookie

--- controller/callbackController.js
+++ controller/callbackController.js
@@ -3,10 +3,11 @@ const {getCallbackParams, generateTokenset} = require("../keycloak/client.js")
 const callback = async (req, res, next) =>{
     try{
         const code_verifier = req.cookies.code_verifier
-        console.log(code_verifier)
+        //console.log(code_verifier)
         const params = await getCallbackParams(req)
         const tokenSet = await generateTokenset("http://localhost:3000/login/cb", params, {code_verifier})
-        console.log(params, tokenSet)
+        //console.log(params, tokenSet)
+        res.cookie("tokenset", tokenSet, {httpOnly: true})
         res.clearCookie("code_verifier")
         res.send("Successfully authenticated")
         next()
