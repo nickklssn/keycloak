9379a24a21c57cf0063ac6acee25a955f3b29328
nickklssn <nickklassen3@gmail.com>
2023-08-10 10:52:45 +0200

    [FIX] set code verifier for cb path

--- controller/loginController.js
+++ controller/loginController.js
@@ -3,14 +3,13 @@ const {
   generateCodeVerifier,
   getAuthUrl,
 } = require("../keycloak/client.js");
-const store = require("store")
 
 const login = (_req, res, next) => {
   try {
     const code_verifier = generateCodeVerifier();
     const code_challenge = generateCodeChallenge(code_verifier);
-    res.cookie("code_verifier", code_verifier, { domain: "webapp.local" ,httpOnly: true});
-    store.set("code_verifier", {code_verifier: code_verifier})
+    res.cookie("code_verifier", code_verifier, { domain: "webapp.local", path:"/login/cb" ,httpOnly: true});
+
     res.redirect(getAuthUrl(code_challenge));
     next();
   } catch (err) {
