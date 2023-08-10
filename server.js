f46b3f8a61878ff718197d033918bbc6192e3483
nickklssn <nickklassen3@gmail.com>
2023-08-10 16:25:41 +0200

    [TASK] route for api fetch

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 
 app.get("/", (_req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/views/login.html");
 });
 
 app.get("/login", login ,async (req, res) => {
@@ -27,7 +27,24 @@ app.get("/login/cb", callback ,async (req, res) => {
 
 });
 
+app.get("/getData", async (req, res) => {
+  try {
+    const access_token = req.cookies.tokenset
+    const response = await fetch("http://api.local:3001/dummyData", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Daten:", error);
+    res.status(500).json({ error: "Fehler beim Abrufen der Daten." });
+  }
+});
+
 app.get("/test",  async(req, res) =>{
+  res.sendFile(__dirname + "/views/dashboard.html");
 })
 
 app.listen(PORT, () => {
