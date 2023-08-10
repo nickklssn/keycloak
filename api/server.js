ada91398216a9d61ef42bab0b75576464eaf2eb8
nickklssn <nickklassen3@gmail.com>
2023-08-10 16:25:13 +0200

    [TASK] add cors config

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const verifyToken = require("./middleware/verifyToken");
 const verifyRole = require("./middleware/verifyRoles");
+const cors = require("cors")
 
 const app = express();
 
@@ -14,6 +15,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(verifyToken)
 
+app.use(cors({
+  origin: "http://webapp.local:3000",
+  credentials: true, 
+}));
+
 app.get("/", verifyRole("app-user") ,(req, res) => {
   res.send("Welcome to the api");
 });
@@ -22,6 +28,12 @@ app.get("/data", (req, res) => {
   res.json({ name: "Nick", age: 23, gender: "m" });
 });
 
+app.get("/dummyData", (req, res) =>{
+  res.json({ name: "Karsten", age: 46, gender: "m" })
+})
+
 app.listen(PORT, () => {
   console.log("Server listen on port 3001");
 });
+
+
