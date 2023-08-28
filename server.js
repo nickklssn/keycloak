ac270fc06cee492319c1c77d4d017a084c5abd9d
nickklssn <nickklassen3@gmail.com>
2023-08-28 13:20:04 +0200

    [TASK] create logout flow

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors")
 const login = require("./controller/loginController.js");
 const callback = require("./controller/callbackController.js");
+const verifyToken = require("./middleware/verifyToken.js");
+const verifyRole = require("./middleware/verifyRoles.js");
+const logout = require("./controller/logoutController.js");
+const { deleteData, queryData } = require("./database/db.js");
 
 const app = express();
 
@@ -12,6 +17,10 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "webapp.local:3000"
+}))
+
 
 
 app.get("/", (_req, res) => {
@@ -20,32 +29,30 @@ app.get("/", (_req, res) => {
 
 app.get("/login", login ,async (req, res) => {
 
-
 });
 
 app.get("/login/cb", callback ,async (req, res) => {
   res.sendFile(__dirname + "/views/dashboard.html");
 });
 
-app.get("/getData", async (req, res) => {
-  try {
-    const access_token = req.cookies.tokenset
-    const response = await fetch("http://api.local:3001/dummyData", {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error("Fehler beim Abrufen der Daten:", error);
-    res.status(500).json({ error: "Fehler beim Abrufen der Daten." });
-  }
+app.get("/getData", /*verifyToken, verifyRole("app-user"),*/ (req, res) => {
+  
+  res.json({text: "Hello from the other side"})
+
 });
 
-app.get("/test",  async(req, res) =>{
-  res.sendFile(__dirname + "/views/dashboard.html");
+app.get("/logout", logout,(req, res) =>{
 })
+
+app.get("/deleteData", async(req, res) =>{
+  await deleteData()
+})
+
+app.get("/showData", async(req, res) =>{
+  const results = await queryData()
+  res.send(results)
+})
+
 
 app.listen(PORT, () => {
   console.log("Server listen on port 3000");
