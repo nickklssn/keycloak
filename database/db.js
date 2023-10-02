37148f453fdc678bed51d0bfb9ef2cc5b385b371
nickklssn <nickklassen3@gmail.com>
2023-10-02 16:07:58 +0200

    [TASK] add try-catch-finally block

--- database/db.js
+++ database/db.js
@@ -1,113 +1,144 @@
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    database: "postgres",
-    user: "postgres",
-    password: "postgres"
-})
+  host: "localhost",
+  port: 5432,
+  database: "postgres",
+  user: "postgres",
+  password: "postgres",
+});
 
-async function queryData(){
-    const client = await pool.connect()
-    const result = await client.query(`SELECT * FROM token;`)
-    client.release()
-    console.log("Das hier sollte das Resultat sein", result)
-    return result
+async function queryData() {
+  let client;
+  try {
+    client = await pool.connect();
+    const result = await client.query(`SELECT * FROM token;`);
+
+    return result;
+  } catch (err) {
+    console.error(err.message)
+  } finally {
+    client.release();
+  }
 }
 
-async function createTable(){
-    const client = await pool.connect()
-        await client.query(`CREATE TABLE IF NOT EXISTS token (
-        "preferred_username" VARCHAR(100),
-        "name" VARCHAR(100),
+async function createTable() {
+  let client;
+  try {
+    client = await pool.connect();
+    await client.query(`CREATE TABLE IF NOT EXISTS token (
         "session_state" VARCHAR(100),
         "access_token" VARCHAR(5000),
         "refresh_token" VARCHAR(5000)
 
-    );`)
-    client.release()
+    );`);
+  } catch (err) {
+    console.error(err.message)
+  } finally {
+    client.release();
+  }
 }
 
-async function insertData(data){
-    await createTable()
-    const client = await pool.connect()
+async function insertData(data) {
+  let client;
+  try {
+    await createTable();
+    client = await pool.connect();
     await client.query(`INSERT INTO token
     VALUES(
-        '${data.preferred_username}',
-        '${data.name}',
         '${data.session_state}',
         '${data.access_token}',
         '${data.refresh_token}'
-    );`)
-    client.release()
+    );`);
+  } catch (err) {
+    console.error(err.message)
+  } finally {
+    client.release();
+  }
 }
 
-async function deleteData(){
-    const client = await pool.connect()
-    await client.query(`DELETE FROM token`)
-    client.release()
-    console.log("Deleted data!")
+async function deleteData() {
+  let client;
+  try {
+    client = await pool.connect();
+    await client.query(`DELETE FROM token`);
+
+    console.log("Deleted all token");
+  } catch (err) {
+    console.error(err.message)
+  } finally {
+    client.release();
+  }
 }
 
-async function deleteToken(token){
-    const client = await pool.connect()
-    await client.query(`DELETE FROM token
+async function deleteToken(token) {
+  let client;
+  try {
+    client = await pool.connect();
+    await client.query(
+      `DELETE FROM token
     WHERE access_token = $1;`,
-            [token])
-    client.release()
-    console.log("Token deleted!")
+      [token]
+    );
+
+    console.log("Token deleted!");
+  } catch (err) {
+    console.error(err.message)
+  } finally {
+    client.release();
+  }
 }
 
 async function getRefreshtoken(data) {
-    const client = await pool.connect();
-    try {
-        const result = await client.query(
-            `SELECT refresh_token FROM token
-            WHERE access_token = $1;`,
-            [data]
-        );
+  let client;
 
-        // Überprüfen, ob Zeilen vorhanden sind
-        if (result.rows.length > 0) {
-            const refresh_token = result.rows[0].refresh_token;
-            console.log(refresh_token);
-            return refresh_token;
-        } else {
-            console.log("Kein Refresh-Token gefunden.");
-            return null; // Oder wir können einen speziellen Wert zurückgeben, um anzuzeigen, dass kein Token gefunden wurde.
-        }
-    } catch (error) {
-        console.error("Fehler beim Abrufen des Refresh-Tokens:", error);
-        return null; // Oder wir können einen speziellen Wert zurückgeben, um den Fehler zu kennzeichnen.
-    } finally {
-        client.release();
+  try {
+    client = await pool.connect();
+    const result = await client.query(
+      `SELECT refresh_token FROM token
+            WHERE access_token = $1;`,
+      [data]
+    );
+    if (result.rows.length > 0) {
+      const refresh_token = result.rows[0].refresh_token;
+      console.log(refresh_token);
+      return refresh_token;
+    } else {
+      console.log("No refresh token found!");
+      return null;
     }
+  } catch (error) {
+    console.error(err.message)
+    return null;
+  } finally {
+    client.release();
+  }
 }
 
 async function updateToken(oldAccessToken, newAccessToken, newRefreshToken) {
-    const client = await pool.connect();
+  let client;
+  try {
+    client = await pool.connect();
     await client.query(
-        `UPDATE token
+      `UPDATE token
         SET access_token = $1,
             refresh_token = $2
         WHERE access_token = $3;
         `,
-        [newAccessToken, newRefreshToken, oldAccessToken]
+      [newAccessToken, newRefreshToken, oldAccessToken]
     );
+  } catch (err) {
+    console.error(err.message)
+  } finally {
     client.release();
+  }
 }
-
-
-
-
-
 
 module.exports = {
-    queryData,
-    insertData,
-    deleteData,
-    getRefreshtoken,
-    updateToken,
-    deleteToken
-}
+  queryData,
+  insertData,
+  deleteData,
+  getRefreshtoken,
+  updateToken,
+  deleteToken,
+};
